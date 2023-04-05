1311eb7f99c6fbec2bf295d049fd4ce583f57f38
biglimp <biglimp@users.noreply.github.com>
2023-04-05 16:25:22 +0200

    Version 2.0.1
    Fixed automatic installation of dependent Python packages

--- processing_umep_provider.py
+++ processing_umep_provider.py
@@ -151,4 +151,4 @@ class ProcessingUMEPProvider(QgsProcessingProvider):
         (version 2.2.1)". This string should be localised. The default
         implementation returns the same string as name().
         """
-        return 'UMEP for Processing, Version 2.0'
+        return 'UMEP for Processing, Version 2.0.1'
