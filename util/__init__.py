2d44e63996d536a8713ecac4088d972679fd434e
Robert Schweizer <robert.schweizer@astrazeneca.com>
2023-11-11 11:48:16 +0100

    fix: Prepend user site dir to sys.path
    Work around https://github.com/qgis/QGIS/issues/55258

--- util/__init__.py
+++ util/__init__.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'xlinfr'
 
+# The .egg packages shipped with QGIS sometimes appear before the user site dir
+# in sys.path. To make sure package versions we install with "--user" take precedence,
+# prepend the user site dir to sys.path before importing other packages.
+# This works around https://github.com/qgis/QGIS/issues/55258
+import site
+import sys
+sys.path.insert(0, site.getusersitepackages())
+
 from qgis.PyQt.QtWidgets import QMessageBox
 from .umep_installer import setup_umep_python
 from qgis.core import Qgis, QgsMessageLog
 # we can specify a version if needed
-try: 
+try:
     import supy as sp
     from supy import __version__ as ver_supy
     QgsMessageLog.logMessage("UMEP - SuPy Version installed: " + ver_supy, level=Qgis.Info)
