3b04ba59cfe2b12df5aaa0768791de11ee849f0d
jeremy <jeremy.bernard@zaclys.net>
2024-04-30 16:09:46 +0200

    small fix

--- functions/URock/GlobalVariables.py
+++ functions/URock/GlobalVariables.py
@@ -73,7 +73,7 @@ JAVA_PATH_FILENAME = "JavaPath.csv"
 # If debug is True, keep intermediate tables (within each process) and save
 # intermediate tables (such as Röckle zones) as GIS file
 DEBUG = True
-ONLY_INITIALIZATION = True
+ONLY_INITIALIZATION = False
 SAVE_ROCKLE_ZONES = False
 MAX_ITERATIONS = 500      # Based on QUIC-URB default values (2021)
 THRESHOLD_ITERATIONS = 1e-4 # Based on QUIC-URB default values (2021)
