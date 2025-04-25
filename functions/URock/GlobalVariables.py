157d237bcacc2f89d4efbca8fbd88e1975b2c295
jeremy <jeremy.bernard@zaclys.net>
2025-04-25 18:46:50 +0200

    Solve new URock issue with densification of downwind facade

--- functions/URock/GlobalVariables.py
+++ functions/URock/GlobalVariables.py
@@ -72,7 +72,7 @@ JAVA_PATH_FILENAME = "JavaPath.csv"
 
 # If debug is True, keep intermediate tables (within each process) and save
 # intermediate tables (such as Röckle zones) as GIS file
-DEBUG = False
+DEBUG = True
 ONLY_INITIALIZATION = False
 SAVE_ROCKLE_ZONES = False
 MAX_ITERATIONS = 500      # Based on QUIC-URB default values (2021)
@@ -89,7 +89,7 @@ DZ = 2
 ALONG_WIND_ZONE_EXTEND = 60
 CROSS_WIND_ZONE_EXTEND = 40
 VERTICAL_EXTEND = 20
-CAV_N_WAKE_FACADE_NPOINTS = 10
+CAV_N_WAKE_FACADE_NPOINTS = 3
 
 # The "perpendicular vortex scheme" for rooftop and displacement zones is activated
 # if the wind angle if more or less 'PERPENDICULAR_THRESHOLD_ANGLE' ° higher
